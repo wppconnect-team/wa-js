54743410fbb527c04374525a0b1f2d64e968ea85
Edgard <edgardmessias@gmail.com>
2021-08-26 19:36:50 -0300

    docs: Added type for Features

--- src/whatsapp/misc/Features.ts
+++ src/whatsapp/misc/Features.ts
@@ -17,6 +17,96 @@
 import { exportModule } from '../exportModule';
 import { EventEmitter } from './EventEmitter';
 
+type FeaturesNames =
+  | 'RESEND_ICON'
+  | 'DELETE_MSG_CLEAR_MEDIA'
+  | 'DEBUG_COMMANDS'
+  | 'MD_BACKEND'
+  | 'MD_ADV'
+  | 'MD_PAYMENT'
+  | 'MD_E2E'
+  | 'MD_VOIP_GROUP'
+  | 'MD_SYNCD_ANTI_TAMPERING'
+  | 'CATALOG_MANAGER'
+  | 'WEB_VOIP_VOICE_CALL'
+  | 'WEB_VOIP_VIDEO_CALL'
+  | 'MEDIA_AUTO_DOWNLOAD'
+  | 'DOWNLOAD_STATUS_THUMB_MMS'
+  | 'MEDIA_EXISTENCE_CHECK'
+  | 'LOG_MISSING_REQUIRED_PROPERTIES'
+  | 'HIGH_QUALITY_VIDEO_THUMBNAILS'
+  | 'HIGH_QUALITY_LINK_PREVIEWS'
+  | 'YOUTUBE_VIDEO_PREVIEW_WITHOUT_BLUR'
+  | 'GROUP_CONVERSATIONS_MEDIA_TOOLTIPS'
+  | 'DROP_LAST_NAME_MENTIONS'
+  | 'RICH_TEXT_INPUT'
+  | 'PTT_DRAFT_ENABLED'
+  | 'PTT_WAVEFORM_SEND'
+  | 'PTT_CONVERSATION_WAVEFORM'
+  | 'SEE_STATUS_VIEWERS'
+  | 'GROUP_CATCH_UP'
+  | 'FORMAT_BULLETED_MSG'
+  | 'ARCHIVE_V2_MD_SUPPORTED'
+  | 'MEDIA_GALLERY_HEADERS'
+  | 'PIP_VIDEO_REDESIGN'
+  | 'LABELS_DISPLAY'
+  | 'VOIP_INDIVIDUAL_OUTGOING'
+  | 'GROUPS_V_3'
+  | 'GROUPS_V_3_CREATE'
+  | 'CHANGE_NUMBER_V_2'
+  | 'QUERY_STATUS_V_3_THUMBNAIL'
+  | 'LIVE_LOCATIONS'
+  | 'QUERY_VNAME'
+  | 'VOIP_INDIVIDUAL_INCOMING'
+  | 'QUICK_REPLIES_QUERY'
+  | 'PAYMENTS'
+  | 'STICKER_PACK_QUERY'
+  | 'LIVE_LOCATIONS_FINAL'
+  | 'LABELS_EDIT'
+  | 'MEDIA_UPLOAD'
+  | 'MEDIA_UPLOAD_RICH_QUICK_REPLIES'
+  | 'VNAME_V_2'
+  | 'VIDEO_PLAYBACK_URL'
+  | 'STATUS_RANKING'
+  | 'VOIP_INDIVIDUAL_VIDEO'
+  | 'THIRD_PARTY_STICKERS'
+  | 'FREQUENTLY_FORWARDED_SETTING'
+  | 'GROUPS_V_4_JOIN_PERMISSION'
+  | 'RECENT_STICKERS'
+  | 'CATALOG'
+  | 'STARRED_STICKERS'
+  | 'VOIP_GROUP_CALL'
+  | 'TEMPLATE_MESSAGE'
+  | 'TEMPLATE_MESSAGE_INTERACTIVITY'
+  | 'EPHEMERAL_MESSAGES'
+  | 'E_2_E_NOTIFICATION_SYNC'
+  | 'RECENT_STICKERS_V_2'
+  | 'RECENT_STICKERS_V_3'
+  | 'USER_NOTICE'
+  | 'SUPPORT'
+  | 'GROUP_UII_CLEANUP'
+  | 'GROUP_DOGFOODING_INTERNAL_ONLY'
+  | 'SETTINGS_SYNC'
+  | 'ARCHIVE_V_2'
+  | 'EPHEMERAL_ALLOW_GROUP_MEMBERS'
+  | 'EPHEMERAL_24_H_DURATION'
+  | 'MD_FORCE_UPGRADE'
+  | 'DISAPPEARING_MODE'
+  | 'EXTERNAL_MD_OPT_IN_AVAILABLE'
+  | 'ARCHIVE_BROADCAST'
+  | 'RECENT_EMOJI_SYNC'
+  | 'STARRED_GIFS'
+  | 'INDEX_RECEIVED_VCARD'
+  | 'STATUS_V3_UI_SENDING'
+  | 'MEDIAS_COUNT'
+  | 'VOIP_VOICE_CALL'
+  | 'DESKTOP_VOIP_VOICE_CALL'
+  | 'DESKTOP_VOIP_VIDEO_CALL'
+  | 'DESKTOP_VOIP_GROUP_VOICE_CALL'
+  | 'DESKTOP_VOIP_GROUP_VIDEO_CALL'
+  | 'KEY_PARTICIPANT'
+  | 'RICH_TEXT';
+
 /**
  * @moduleID 77188
  * @whatsapp 2.2132.6
@@ -27,18 +117,18 @@ declare class FeatureClass extends EventEmitter {
   VF: { [key: string]: string };
   silenceChangeEvents: boolean;
   triggerFeaturesChangedDebounced: any;
-  features: { [key: string]: true };
+  features: { [key in FeaturesNames]: boolean };
   proto: [number, number];
   setVersion(version: [number, number]): void;
   setPlatform(platform: string): void;
   setFeatureFromFlags(e?: any, t?: any, r?: any): any;
-  setFeature(e?: any, t?: any): any;
-  setFeatures(e?: any): any;
-  supportsFeature(feature: string): boolean;
+  setFeature(feature: FeaturesNames, supported: boolean): any;
+  setFeatures(features: { [key in FeaturesNames]: boolean }): any;
+  supportsFeature(feature: FeaturesNames): boolean;
   supportsFeatureFromFlags(flag: any): boolean;
-  supportsAllFeatures(...features: string[]): boolean;
-  supportsAnyFeature(...features: string[]): boolean;
-  supportsAnyFeature(...features: string[]): boolean;
+  supportsAllFeatures(...features: FeaturesNames[]): boolean;
+  supportsAnyFeature(...features: FeaturesNames[]): boolean;
+  supportsAnyFeature(...features: FeaturesNames[]): boolean;
   resetFeatures(): void;
   lt(version: [number, number]): boolean;
   ltr(version: [number, number]): boolean;
