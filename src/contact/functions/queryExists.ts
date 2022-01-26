bf29270d27c16ca1348742ed532e96f655566349
Edgard <edgardmessias@gmail.com>
2022-01-26 10:51:32 -0300

    fix: Fixed WPP.contact.queryExists function for false positive

--- src/contact/functions/queryExists.ts
+++ src/contact/functions/queryExists.ts
@@ -38,6 +38,19 @@ export interface QueryExistsResult {
 
 const cache = new Map<string, QueryExistsResult | null>();
 
+/**
+ * Check if the number exists and what is correct ID
+ *
+ * This help to identify numbers with nine digit in Brazil
+ *
+ * @example
+ * ```javascript
+ * const result = await WPP.contact.queryExists('<number>@c.us');
+ * console.log(result.wid); // Correct ID
+ * ```
+ *
+ * @category Chat
+ */
 export async function queryExists(
   contactId: string | Wid
 ): Promise<QueryExistsResult | null> {
@@ -49,28 +62,32 @@ export async function queryExists(
     return cache.get(id)!;
   }
 
-  let result = await sendQueryExists(wid).catch(() => null);
+  let result: QueryExistsResult | null = null;
 
-  if (!result) {
-    const query = await Wap.queryExist(id);
-    if (query.status === 200) {
-      result = {
-        wid: query.jid,
-        biz: query.biz || false,
-      };
+  const query = await Wap.queryExist(id);
+  if (query.status === 200) {
+    result = {
+      wid: query.jid,
+      biz: query.biz || false,
+    };
 
-      // @todo: Migrate condition to isDisappearingModeEnabled()
-      if (result) {
-        const disappearing = await Wap.queryDisappearingMode(wid);
+    // @todo: Migrate condition to isDisappearingModeEnabled()
+    if (result) {
+      const disappearing = await Wap.queryDisappearingMode(wid).catch(
+        () => null
+      );
 
-        if (disappearing.status === 200) {
-          result.disappearingMode = {
-            duration: disappearing.duration!,
-            settingTimestamp: disappearing.settingTimestamp!,
-          };
-        }
+      if (disappearing?.status === 200) {
+        result.disappearingMode = {
+          duration: disappearing.duration!,
+          settingTimestamp: disappearing.settingTimestamp!,
+        };
       }
     }
+  }
+
+  if (!result) {
+    result = await sendQueryExists(wid).catch(() => null);
   }
 
   cache.set(id, result);
