e3189021673f866d3c6ebc82af12794d07241614
Edgard <edgardmessias@gmail.com>
2022-06-09 14:19:33 -0300

    feat: Aded call.incoming_call event

--- src/eventEmitter/eventTypes.ts
+++ src/eventEmitter/eventTypes.ts
@@ -1,5 +1,5 @@
 /*!
- * Copyright 2021 WPPConnect Team
+ * Copyright 2022 WPPConnect Team
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,18 +15,21 @@
  */
 
 import { BlocklistEventTypes } from '../blocklist/events/eventTypes';
+import { CallEventTypes } from '../call/events/eventTypes';
 import { ChatEventTypes } from '../chat/events/eventTypes';
 import { ConnEventTypes } from '../conn/events/eventTypes';
 import { StatusEventTypes } from '../status/events/eventTypes';
 import { WebpackEvents } from '../webpack/eventTypes';
 
 export { BlocklistEventTypes } from '../blocklist/events/eventTypes';
+export { CallEventTypes } from '../call/events/eventTypes';
 export { ChatEventTypes } from '../chat/events/eventTypes';
 export { ConnEventTypes } from '../conn/events/eventTypes';
 export { StatusEventTypes } from '../status/events/eventTypes';
 export { WebpackEvents } from '../webpack/eventTypes';
 
 export type EventTypes = BlocklistEventTypes &
+  CallEventTypes &
   ChatEventTypes &
   ConnEventTypes &
   StatusEventTypes &
