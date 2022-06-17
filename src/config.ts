19079d7c0e7e7c3440e94275d4536226c8f324ea
sergey <110@mobilon.ru>
2022-06-17 17:30:16 +0700

    feat: Usage Google Analytics optional, can set own GA track id

--- src/config.ts
+++ src/config.ts
@@ -22,7 +22,9 @@
  * // Global variable before injection
  * WPPConfig = {
  *   deviceName: 'WPPConnect',
- *   liveLocationLimit: 10
+ *   liveLocationLimit: 10,
+ *   disableGoogleAnalytics: false,
+ *   googleAnalyticsId: 'G-XXXXXXXXXX'
  * };
  * ```
  */
@@ -37,11 +39,23 @@ export interface Config {
    * Number of last chats to check live location after a page reload
    */
   liveLocationLimit: number;
+
+  /**
+   * Disable Google Analytics tracking
+   */
+  disableGoogleAnalytics: boolean;
+
+  /**
+   * Google Analytics Id
+   */
+  googleAnalyticsId: string;
 }
 
 export const defaultConfig: Config = {
   deviceName: false,
   liveLocationLimit: 10,
+  disableGoogleAnalytics: false,
+  googleAnalyticsId: 'G-MTQ4KY110F',
 };
 
 export const config: Config = defaultConfig;
