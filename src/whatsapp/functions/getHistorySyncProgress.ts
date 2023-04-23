8ba04e891eb8d06be5a80b550b74fba79dc70f18
Edgard <edgardmessias@gmail.com>
2023-04-23 19:43:18 -0300

    fix: Fixed exported getHistorySyncProgress function

--- src/whatsapp/functions/getHistorySyncProgress.ts
+++ src/whatsapp/functions/getHistorySyncProgress.ts
@@ -1,5 +1,5 @@
 /*!
- * Copyright 2022 WPPConnect Team
+ * Copyright 2023 WPPConnect Team
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,5 +27,5 @@ exportModule(
   {
     getHistorySyncProgress: 'getHistorySyncProgress',
   },
-  (m) => m.getHistorySyncProgress
+  (m) => m.getHistorySyncProgress && !m.getHistorySyncLogDetailsString
 );
