c3466d1120483cdb4355bdbb46da29dbc96c0a00
Víctor Phelipe <32254362+vphelipe@users.noreply.github.com>
2023-04-24 10:05:34 -0300

    Update getNumChatsPinned.ts

--- src/whatsapp/functions/getNumChatsPinned.ts
+++ src/whatsapp/functions/getNumChatsPinned.ts
@@ -5,7 +5,7 @@ import { exportModule } from '../exportModule';
  * @whatsapp 695547 >= 2.2222.8
  * @whatsapp 925080 >= 2.2228.4
  */
-export declare function numPinned(value: any): any;
+export declare function getNumChatsPinned(value: any): any;
 
 exportModule(
   exports,
