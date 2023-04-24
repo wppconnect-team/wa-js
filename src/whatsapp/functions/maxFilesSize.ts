2a96710cf6ab49ce8973751faf236478b06275fc
Víctor Phelipe <32254362+vphelipe@users.noreply.github.com>
2023-04-23 22:25:57 -0300

    Update maxFilesSize.ts

--- src/whatsapp/functions/maxFilesSize.ts
+++ src/whatsapp/functions/maxFilesSize.ts
@@ -5,17 +5,7 @@ import { exportModule } from '../exportModule';
  * @whatsapp 695547 >= 2.2222.8
  * @whatsapp 925080 >= 2.2228.4
  */
-export declare function maxFilesSize(value: any): any;
 export declare function numPinned(value: any): any;
-
-
-exportModule(
-  exports,
-  {
-    maxFilesSize: 'getMaxFilesSizeServerProp',
-  },
-  (m) => m.getMaxFilesSizeServerProp && m.ServerProps
-);
 
 exportModule(
   exports,
