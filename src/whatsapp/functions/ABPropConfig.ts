7f7ae9b279a7266bc8f34e6bd190668fe7f9c140
Víctor Phelipe <32254362+vphelipe@users.noreply.github.com>
2023-04-23 22:32:56 -0300

    feat: Native high quality link preview

---
+++ src/whatsapp/functions/ABPropConfig.ts
@@ -0,0 +1,17 @@
+import { exportModule } from '../exportModule';
+
+/**
+ * @whatsapp 95547
+ * @whatsapp 695547 >= 2.2222.8
+ * @whatsapp 925080 >= 2.2228.4
+ */
+ 
+export declare function ABPropConfig(value: any): any;
+
+exportModule(
+  exports,
+  {
+    ABPropConfig: 'getABPropConfigValue',
+  },
+  (m) => m.getABPropConfigValue
+);
