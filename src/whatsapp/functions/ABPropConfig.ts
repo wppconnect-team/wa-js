233b23786126b50def74447635783f94043f203c
Víctor Phelipe <32254362+vphelipe@users.noreply.github.com>
2023-04-24 09:37:47 -0300

    Update src/whatsapp/functions/ABPropConfig.ts
    Co-authored-by: Edgard Lorraine Messias <edgardmessias@gmail.com>

--- src/whatsapp/functions/ABPropConfig.ts
+++ src/whatsapp/functions/ABPropConfig.ts
@@ -11,7 +11,7 @@ export declare function ABPropConfig(value: any): any;
 exportModule(
   exports,
   {
-    ABPropConfig: 'getABPropConfigValue',
+    getABPropConfigValue: 'getABPropConfigValue',
   },
   (m) => m.getABPropConfigValue
 );
