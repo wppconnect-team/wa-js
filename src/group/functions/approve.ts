0661f0290e547d0104b6c062da8d87a41cde4dfa
Cleiton Carvalho <cleitoncosta83@gmail.com>
2023-04-29 10:50:32 -0300

    feat: Added WPP.group.approve function

---
+++ src/group/functions/approve.ts
@@ -0,0 +1,55 @@
+/*!
+ * Copyright 2023 WPPConnect Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { assertWid } from '../../assert';
+import { WPPError } from '../../util';
+import { Wid } from '../../whatsapp';
+import { membershipApprovalRequestAction } from '../../whatsapp/functions';
+
+/**
+ * Approve a membership request to group
+ *
+ * @example
+ * ```javascript
+ * await WPP.group.approve(12345645@g.us, 5554999999999@c.us);
+ * ```
+ *
+ * @category Group
+ */
+export async function approve(
+  groupId: string | Wid,
+  membershipIds: (string | Wid) | (string | Wid)[]
+): Promise<
+  {
+    error: any;
+    wid: Wid;
+  }[]
+> {
+  groupId = assertWid(groupId);
+  if (!Array.isArray(membershipIds)) {
+    membershipIds = [membershipIds];
+  }
+  const wids = membershipIds.map(assertWid);
+
+  try {
+    return await membershipApprovalRequestAction(groupId, wids, 'Approve');
+  } catch (error) {
+    throw new WPPError(
+      'error_on_accept_membership_request',
+      `Error on accept member on group ${groupId.toString()}`
+    );
+  }
+}
