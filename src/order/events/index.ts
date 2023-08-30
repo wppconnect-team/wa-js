cadd0bd8e9170b67c6597868d286b8c91b5d8c19
Cleiton Carvalho <cleitoncosta83@gmail.com>
2023-08-30 20:24:56 -0300

    feat: Added order.payment_status event

--- src/order/events/index.ts
+++ src/order/events/index.ts
@@ -1,5 +1,5 @@
 /*!
- * Copyright 2023 WPPConnect Team
+ * Copyright 2021 WPPConnect Team
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,5 +14,4 @@
  * limitations under the License.
  */
 
-export * from './events';
-export * from './functions';
+import './registerUpdateOrderEvent';
