470baa4231572cfdb98782d195bebc437599da59
Cleiton Carvalho <cleitoncosta83@gmail.com>
2023-08-30 21:27:33 -0300

    feat: Added whatsapp function addAndSendMessageEdit

--- src/whatsapp/functions/index.ts
+++ src/whatsapp/functions/index.ts
@@ -14,6 +14,7 @@
  * limitations under the License.
  */
 
+export * from './addAndSendMessageEdit';
 export * from './addAndSendMsgToChat';
 export * from './addToLabelCollection';
 export * from './blockContact';
