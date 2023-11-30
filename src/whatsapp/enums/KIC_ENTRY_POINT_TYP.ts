77298e14d34e96c92d5257507b260419ca047c21
Cleiton Carvalho <cleitoncosta83@gmail.com>
2023-11-30 13:01:42 -0300

    feat: Added WPP.chat.keepMessage function (close #1142) (#1517)
    * feat: Added WPP.chat.keepMessage function
    
    * fix: Typos fix
    
    * fix: Fixed imports

--- src/whatsapp/enums/KIC_ENTRY_POINT_TYP.ts
+++ src/whatsapp/enums/KIC_ENTRY_POINT_TYP.ts
@@ -1,5 +1,5 @@
 /*!
- * Copyright 2021 WPPConnect Team
+ * Copyright 2023 WPPConnect Team
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,11 +14,24 @@
  * limitations under the License.
  */
 
-export * from './ACK';
-export * from './CALL_STATES';
-export * from './GROUP_SETTING_TYPE';
-export * from './KIC_ENTRY_POINT_TYP';
-export * from './LogoutReason';
-export * from './MSG_TYPE';
-export * from './OUTWARD_TYPES';
-export * from './SendMsgResult';
+import { exportModule } from '../exportModule';
+
+/**
+ * @whatsapp 982015
+ */
+export declare enum KIC_ENTRY_POINT_TYPE {
+  CHAT_INFO = 1,
+  SEARCH = 2,
+  CHAT = 3,
+  MEDIA = 4,
+  DOCS = 5,
+  LINKS = 6,
+}
+
+exportModule(
+  exports,
+  {
+    KIC_ENTRY_POINT_TYPE: 'KIC_ENTRY_POINT_TYPE',
+  },
+  (m) => m.KIC_ENTRY_POINT_TYPE
+);
