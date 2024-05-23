7c50f80772c653bd41bb2e4d13d7a00e790b19fc
Cleiton Carvalho <cleitoncosta83@gmail.com>
2024-05-23 20:17:29 -0300

    fix: Added version in compiled files (close #1928)

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,63 @@
 const path = require('path');
 const webpack = require('webpack');
+const TerserPlugin = require('terser-webpack-plugin');
 const packageJSON = require('./package.json');
 
-module.exports = {
-  entry: './src/index.ts',
-  mode: 'production',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
+module.exports = (env, argv) => {
+  const isDevelopment = argv.mode === 'development';
+  return {
+    entry: './src/index.ts',
+    mode: isDevelopment ? 'development' : 'production',
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+      fallback: {
+        fs: false,
       },
+    },
+    output: {
+      filename: 'wppconnect-wa.js',
+      path: path.resolve(__dirname, 'dist'),
+      library: {
+        name: 'WPP',
+        type: 'global',
+      },
+    },
+    ...(!isDevelopment ? { optimization: {
+      minimize: true,
+      minimizer: [
+        new TerserPlugin({
+          terserOptions: {
+            format: {
+              comments:(astNode, comment) => /wa-js v/.test(comment.value),
+            },
+          },
+          extractComments: true,
+        }),
+      ],
+    } } : {})
+    ,
+    plugins: [
+      new webpack.ProvidePlugin({
+        Buffer: ['buffer', 'Buffer'],
+      }),
+      new webpack.DefinePlugin({
+        __VERSION__: `'${packageJSON.version}'`,
+        __SUPPORTED_WHATSAPP_WEB__: `'${packageJSON.engines['whatsapp-web']}'`,
+      }),
+      new webpack.BannerPlugin({
+        banner: `/*! wppconnect-team/wa-js v${packageJSON.version} */\n`,
+        entryOnly: true,
+        raw: true
+      })
     ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-    fallback: {
-      fs: false,
-    },
-  },
-  output: {
-    filename: 'wppconnect-wa.js',
-    path: path.resolve(__dirname, 'dist'),
-    library: {
-      name: 'WPP',
-      type: 'global',
-    },
-  },
-  plugins: [
-    new webpack.ProvidePlugin({
-      Buffer: ['buffer', 'Buffer'],
-    }),
-    new webpack.DefinePlugin({
-      __VERSION__: `'${packageJSON.version}'`,
-      __SUPPORTED_WHATSAPP_WEB__: `'${packageJSON.engines['whatsapp-web']}'`,
-    }),
-  ],
+  };
 };
