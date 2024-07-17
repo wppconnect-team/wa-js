6271cc94c1cc1a2757e75adb1261067263255ed2
icleitoncosta <cleitoncosta83@gmail.com>
2024-07-17 19:54:47 -0300

    feat: Added WPP.cart.remove function

---
+++ src/cart/functions/remove.ts
@@ -0,0 +1,44 @@
+/*!
+ * Copyright 2024 WPPConnect Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Remove a product in cart
+ *
+ * @example
+ * ```javascript
+ * const cart = WPP.cart.remove('[number]@c.us', '6987301181294productId');
+ * ```
+ *
+ * @category Cart
+ */
+
+import { WPPError } from '../../util';
+import { CartModel, CartStore } from '../../whatsapp';
+import { deleteProductFromCart } from '../../whatsapp/functions';
+
+export async function remove(
+  chatId: string,
+  productId: string
+): Promise<CartModel | undefined> {
+  if (!chatId || !productId) {
+    throw new WPPError(
+      'send_required_params',
+      `For update item in cart send chatId and productId`
+    );
+  }
+  await deleteProductFromCart(chatId, productId);
+  return CartStore.findCart(chatId);
+}
