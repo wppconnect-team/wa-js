495892653c1c8c8eaf891207544649da43fe467d
C. Henrique R. Kw. <68700320+carlosmixz@users.noreply.github.com>
2025-07-09 11:09:38 -0300

    fix: Fixed getGroupInfoFromInviteCode (close #2825, close #2827, close #2828) (#2829)
    * Interface update on sendQueryGroupInvite.ts
    
    * Updated interface on getGroupInfoFromInviteCode.ts

--- src/whatsapp/functions/sendQueryGroupInvite.ts
+++ src/whatsapp/functions/sendQueryGroupInvite.ts
@@ -18,40 +18,43 @@ import { Wid } from '..';
 import { exportModule } from '../exportModule';
 
 export interface QueryGroupInviteResult {
-  announce: boolean;
-  creation: number;
-  /** description of the group; linebreaks are formatted using `"\n"` */
-  desc: string;
-  descId: string;
-  descOwner?: Wid;
-  descTime: number;
-  id: Wid;
-  noFrequentlyForwarded: boolean;
-  owner?: Wid;
-  parent: boolean;
-  participants: {
+  groupInfo: {
+    announce: boolean;
+    creation: number;
+    /** description of the group; linebreaks are formatted using `"\n"` */
+    desc: string;
+    descId: string;
+    descOwner?: Wid;
+    descTime: number;
     id: Wid;
-    isAdmin: boolean;
-    isSuperAdmin: boolean;
-  }[];
-  pvId?: string;
-  restrict: boolean;
-  /** how many members the group currently has */
-  size: number;
+    noFrequentlyForwarded: boolean;
+    owner?: Wid;
+    parent: boolean;
+    participants: {
+      id: Wid;
+      isAdmin: boolean;
+      isSuperAdmin: boolean;
+    }[];
+    pvId?: string;
+    restrict: boolean;
+    /** how many members the group currently has */
+    size: number;
+    status: number;
+    /** title of the group */
+    subject: string;
+    subjectOwner?: Wid;
+    subjectTime: number;
+    support: boolean;
+    suspended: boolean;
+    isParentGroup: boolean;
+    isParentGroupClosed: boolean;
+    defaultSubgroup: boolean;
+    generalSubgroup: boolean;
+    membershipApprovalMode: boolean;
+    isLidAddressingMode: boolean;
+    generalChatAutoAddDisabled: boolean;
+  };
   status: number;
-  /** title of the group */
-  subject: string;
-  subjectOwner?: Wid;
-  subjectTime: number;
-  support: boolean;
-  suspended: boolean;
-  isParentGroup: boolean;
-  isParentGroupClosed: boolean;
-  defaultSubgroup: boolean;
-  generalSubgroup: boolean;
-  membershipApprovalMode: boolean;
-  isLidAddressingMode: boolean;
-  generalChatAutoAddDisabled: boolean;
 }
 
 /** @whatsapp 10790
