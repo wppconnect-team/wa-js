39a84391f866f8f52d49924e3f4117cc092fa850
waTools-br <watoolsbr@gmail.com>
2025-08-29 19:26:26 -0300

    fix: Fixed getMyDeviceId >= 2.3000.1026498050

--- src/whatsapp/misc/UserPrefs.ts
+++ src/whatsapp/misc/UserPrefs.ts
@@ -21,6 +21,7 @@ import { Wid } from './Wid';
  * @whatsapp 459857 >= 2.2310.5
  */
 export declare namespace UserPrefs {
+  function getMeDevicePn(...args: any[]): any;
   function assertGetMe(): Wid;
   function assertGetMeUser(): Wid;
   function clearGetMaybeMeUserCache(...args: any[]): any;
@@ -28,7 +29,6 @@ export declare namespace UserPrefs {
   function getMaybeMeLid(...args: any[]): any;
   function getMaybeMeLidUser(...args: any[]): any;
   function getMaybeMeUser(): Wid;
-  function getMe(...args: any[]): any;
   function getMePNandLIDWids(...args: any[]): any;
   function getMeUser(): Wid;
   function isMeAccount(...args: any[]): any;
@@ -39,6 +39,12 @@ export declare namespace UserPrefs {
   function setMe(...args: any[]): any;
   function setMeDisplayName(...args: any[]): any;
   function setMeLid(...args: any[]): any;
+
+  /**
+   * @deprecated
+   * @whatsapp 498050 >= 2.3000.1026
+   **/
+  function getMe(...args: any[]): any;
 }
 
 exportModule(exports, 'UserPrefs', (m) => m.getMaybeMeUser);
