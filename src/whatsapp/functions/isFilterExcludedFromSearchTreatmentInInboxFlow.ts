154139f10f6db5f789f68e9f035a69bcb53e3171
Mauricio N. Ferreira <contato@ferreiramauricio.com>
2025-11-24 22:47:30 -0300

    fix: reset custom filter on filter type switch and update return type to boolean

--- src/whatsapp/functions/isFilterExcludedFromSearchTreatmentInInboxFlow.ts
+++ src/whatsapp/functions/isFilterExcludedFromSearchTreatmentInInboxFlow.ts
@@ -18,7 +18,7 @@ import { exportModule } from '../exportModule';
 
 export declare function isFilterExcludedFromSearchTreatmentInInboxFlow(
   type?: string
-): void;
+): boolean;
 
 exportModule(
   exports,
